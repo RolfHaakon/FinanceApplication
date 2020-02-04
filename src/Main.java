12647c4df4c2cdc0e3cc0d0e8207920f477562f1
Rolf <30429371+RolfHaakon@users.noreply.github.com>
2020-02-04 01:18:04 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        String API_Key = "Q2G14KIXC3ZZA8G5";
+        String API_Key = "";
         String urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo";
         //GetRequest.StockData(urlString);
 
